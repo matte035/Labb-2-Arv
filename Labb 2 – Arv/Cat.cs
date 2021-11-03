4c628d406b3691df83ff436c8ba54e199d3c29e5
Mattias Kleman <mattias.kleman@gmail.com>
2021-11-03 14:29:40 +0100

    Add project files.

---
+++ Labb 2 – Arv/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_2___Arv
+{
+    public class Cat : Djur
+    {
+        public int legs;
+
+        public void hunt()
+        {
+
+        }
+    }
+    
+}
---
+++ Labb 2 – Arv/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Labb_2___Arv
+{
+    public class Djur
+    {
+        public int Age;
+        public int Height;
+        public int Weight;
+        public string Gender;
+        public string Food;
+
+        public void Walking()
+        {
+            Console.WriteLine();
+        }
+        public void Jump()
+        {
+            Console.WriteLine();
+        }
+        public void Eating()
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Cat C = new Cat();
+
+           
+        }
+    }
+}
